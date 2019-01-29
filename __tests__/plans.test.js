1457af8ec5db73f43205fd8520400184501f263b
Jackson Onyango <jackson@medsender.com>
2019-01-29 16:44:31 +0300

    Refactor plansroute test

--- __tests__/plans.test.js
+++ __tests__/plans.test.js
@@ -1,8 +1,25 @@
 const request = require('supertest');
 const app = require('../app.js');
+const { Plan } = require('../db')
 /* eslint no-undef:0 */
 
 describe('Test plan routes', () => {
+  beforeAll((done) => {
+  Plan.sync({ force: true })
+    .then(() => {
+      request(app)
+        .post('/api/v1/plans')
+        .send({
+          planName: 'Jane',
+          type: 'recurssive',
+        })
+        .end(() => {
+          done()
+        });
+    });
+});
+
+
   test('A plan can be created', (done) => {
     const payload = { planName: 'Silver', type: 'recurssive'};
     request(app)
